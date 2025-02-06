ed2c43e55c3871a7c29e950030928f1497d3b6b7
Monishkar223 <monishkar223@gmail.com>
2025-02-06 18:21:51 +0530

    Update DAY_12.py

--- DAY_12.py
+++ DAY_12.py
@@ -1,7 +1,7 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         
+        d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}        
         a = 0
         b = 0
         n = len(s)
